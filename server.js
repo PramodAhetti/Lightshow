b2c0aabfe9ac6ebd59c5f6a3e6aadfacac5de7e2
PramodAhetti <pramodahetti05@gmail.com>
2023-11-23 01:09:46 +0530

    upgraded the 3*3 to 5*5 and display hi

--- server.js
+++ server.js
@@ -27,21 +27,25 @@ io.on('connection',user=>{
 
 
 
+const pattern = [
+    [1, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+    [1, 1, 1, 1, 1, 0, 0, 1, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+    [1, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+];
 
-let pattern=[[0,1,0],
-             [1,0,1],
-             [0,1,0],
-             [1,0,1],
-             [1,1,1],
-             [0,0,0]];
+
+
+
 function display(colour){
   
-        let i=1;
-        for(i;i<(pattern.length-1);i+=3){
-            for(let j=1;j<(pattern[0].length-1);j+=3){
+        let i=2;
+        for(i;i<(pattern.length-1);i+=5){
+            for(let j=2;j<(pattern[0].length-1);j+=5){
                 let char=97;
-                for(let k=i-1;k<=i+1;k++){
-                    for(let m=j-1;m<=j+1;m++){
+                for(let k=i-2;k<=i+2;k++){
+                    for(let m=j-2;m<=j+2;m++){
                         if(pattern[k][m]==1){
                             let box=String.fromCharCode(char);
                             
