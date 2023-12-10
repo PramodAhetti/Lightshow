622c82119c6a2c42773d80a2c729e36785eef1bb
PramodAhetti <pramodahetti05@gmail.com>
2023-12-10 12:56:34 +0530

    added moving text

--- routes/display.js
+++ routes/display.js
@@ -9,28 +9,44 @@ const hashMap=socketServer.hashMap;
 
 
 
-function display(pattern){
-            let color=[255,25,2];
-            let i=2;
-            for(let j=2;j<(pattern[0].length-1);j+=5){
-                let char=97;
-                for(let k=i-2;k<=i+2;k++){
-                    for(let m=j-2;m<=j+2;m++){
-                        if(pattern[k][m]==1){
-                            let box=String.fromCharCode(char);
-                            
-                            console.log(char,box,`${i} ${j}`)
-                            io.to(hashMap[`${i} ${j}`]).emit('colour',box,[color[0],color[1],color[2]]);
-                           
-                        }
-                        color[0]-=10;
-                        color[2]+=10;
-                        char++;
+function display(pattern) {
+    let color = [255, 25, 2];
+    let i = 2;
+
+    function processIteration(it) {
+        console.log('here');
+        io.emit('reset', 1);
+        let index = 2;
+
+        for (let j = it; j < pattern[0].length - 1; j += 5) {
+            let char = 97;
+
+            for (let k = i - 2; k <= i + 2; k++) {
+                for (let m = j - 2; m <= j + 2; m++) {
+                    if (pattern[k][m] == 1) {
+                        let box = String.fromCharCode(char);
+
+                        console.log(i, index);
+                        io.to(hashMap[`${i} ${index}`]).emit('colour', box, [color[0], color[1], color[2]]);
                     }
+
+                    char++;
                 }
-                console.log("here",j);
             }
+
+            index += 5;
         }
+    }
+
+    for (let it = 2; it < pattern[0].length; it++) {
+        setTimeout(() => {
+            processIteration(it);
+        }, it * 50);
+    }
+}
+
+
+
         
 
 
@@ -42,12 +58,16 @@ function display(pattern){
 
 
 router.get('/',(req,res)=>{
+    io.emit('reset',1);
     let msg=req.query.msg;
     let pixels=[[],[],[],[],[]];
     msg=msg.toUpperCase();
-    console.log(msg);
+
     for(let i=0;i<msg.length;i++){
         const current=LTM[msg[i]];
+        for(let j=0;j<5;j++){
+                pixels[j].push(0);
+        }
         for(let j=0;j<5;j++){
             for(let k=0;k<5;k++){
                 pixels[j].push(current[j][k]);
@@ -55,7 +75,9 @@ router.get('/',(req,res)=>{
         }
     }
     display(pixels);
-    console.log(pixels);
+
+
+
     res.send("done");
 })
 
